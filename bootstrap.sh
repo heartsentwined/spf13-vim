4dbb9a4873d44726d854cce361ecef31fddef788
heartsentwined <heartsentwined@cogito-lab.com>
2012-10-04 23:43:49 +0800

    simplified Vundle installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,4 +53,4 @@ if [ ! -e $HOME/.vim/bundle/vundle ]; then
 fi
 
 echo "update/install plugins using Vundle"
-vim -u $endpath/.vimrc.bundles - +BundleInstall! +BundleClean +qall
+vim +BundleInstall! +BundleClean +qall
